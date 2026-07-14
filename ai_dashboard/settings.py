595ad7959c66f56d8dbe7676b216a636474f1ff2
Prasoon <pdwivedi9090@gmail.com>
2026-07-14 22:40:57 +0530

    added production deployment configuration

--- ai_dashboard/settings.py
+++ ai_dashboard/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure--_q^%o+t_am-mp&whivb02)#_f@rc@)6-r8neqx#rw3!usg7w2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".onrender.com",
+]
 
 
 # Application definition
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = [
 ]
